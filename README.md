53c774f2af8d90809af40c24205238e27e66efbf
Camilo Nova <camilo.nova@axiacore.com>
2015-04-24 11:03:44 -0500

    Added setuptools

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # mopidy-grooveshark
 Play music from Grooveshark on mopidy
+
+Seems package on pypi is outdated, we need to check first.
+pip install https://github.com/koehlma/pygrooveshark/archive/master.zip
+
+Install it from git
+pip install https://github.com/camilonova/mopidy-grooveshark/archive/master.zip
---
+++ setup.py
@@ -0,0 +1,42 @@
+from __future__ import unicode_literals
+
+import re
+from setuptools import setup, find_packages
+
+
+def get_version(filename):
+    content = open(filename).read()
+    metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", content))
+    return metadata['version']
+
+
+setup(
+    name='Mopidy-Grooveshark',
+    version=get_version('mopidy_grooveshark/__init__.py'),
+    url='https://github.com/dz0ny/mopidy-grooveshark',
+    license='MIT',
+    author='Camilo Nova',
+    author_email='camilo.nova@gmail.com',
+    description='Mopidy extension that plays sound from Grooveshark',
+    long_description=open('README.rst').read(),
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+        'setuptools',
+        'Mopidy >= 1.0',
+    ],
+    entry_points={
+        'mopidy.ext': [
+            'grooveshark = mopidy_grooveshark:Extension',
+        ],
+    },
+    classifiers=[
+        'Environment :: No Input/Output (Daemon)',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Multimedia :: Sound/Audio :: Players',
+    ],
+)
